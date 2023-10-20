f0bbce5590c3db65347c80ca607d8f20050034c4
takumi <tkngtkmrwgnnns@gmail.com>
2023-10-20 20:46:06 +0900

    make finger ditection

---
+++ scripts/finger.py
@@ -0,0 +1,24 @@
+from hand_detect import finger_direction
+
+# args : camera_id (default: 0)
+# yield : direction ("R" | "L" | None)
+# ESC to exit
+
+r = 0
+l = 0
+
+for direction in finger_direction():
+    print(direction)
+
+    if direction  == "R":
+        r += 1
+        
+    if r > 10:
+            print("right")
+            break
+        
+    elif direction  == "L":
+        l += 1
+    if l > 10:
+            print("right")
+            break
