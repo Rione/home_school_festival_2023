af896ab0d33268fc9594670703c1039a87cea45d
reina <ryugureina314@gmail.com>
2023-10-24 11:07:49 +0900

    update: Controller.py

--- scripts/Controller.py
+++ scripts/Controller.py
@@ -5,16 +5,18 @@ class Controller():
     """
     A class for managing robot's movement and the connections between the nodes. 
     """
-    def __init__(self) -> None:
+    def __init__(self):
         self.bagColor: str = None    # 紙袋の色
         self.direction: str = None   # 進行方向
     
-    def SetBagColor(self) -> None:
-        if(self.bagColor): return
-        else:
-            self.bagColor = rospy.wait_for_message('audio_processing', String, timeout=None)
+    def ListenToBagColor(self):
+        """
+        Override current bag color.
+        """
+        self.bagColor = rospy.wait_for_message('audio_processing', String, timeout=None)
 
-    def SetDirection(self) -> None:
-        if(self.direction): return
-        else:
-            self.direction = rospy.wait_for_message('finger_direction', String, timeout=None)
+    def ListenToDirection(self):
+        """
+        Override current direction.
+        """
+        self.direction = rospy.wait_for_message('finger_direction', String, timeout=None)
