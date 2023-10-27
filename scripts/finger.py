60dc096156266cd664b3c56358a7c341855d1cc1
Mitsuki1025 <menka123nachi16@icloud.com>
2023-10-27 19:22:25 +0900

    improve error

--- scripts/finger.py
+++ scripts/finger.py
@@ -3,30 +3,31 @@ from hand_detect import finger_direction
 # args : camera_id (default: 0)
 # yield : direction ("R" | "L" | None)
 # ESC to exit
+import sys
+from time import sleep
 
-n = -1
-dir_list = ['0','0','0','0','0','0','0','0','0','0'] #からのリストを作った
+dir_list = [] #からのリストを作った
 
-for direction in finger_direction(): #連続してdirectionを出してる
-  for val in (direction or []):
-   pass
+while True:
+  for direction in finger_direction(): #連続してdirectionを出してる
+    for val in (direction or []): #None 
+      pass
 
-   while n<=8:
-    n = n+1
-    dir_list.insert(0+n,direction)
-    rs = sum([s.endswith('R') for s in dir_list])
-    ls = sum([s.endswith('L') for s in dir_list])
-    print(dir_list)  
-    if rs == 10:
-      print('右')
-      break
-    elif ls == 10:
-      print('左')
-      break
-    else:
-       print('認識できませんでした')
+      dir_list.append(direction)
+      rs = sum([s.endswith('R') for s in dir_list])
+      ls = sum([s.endswith('L') for s in dir_list])
+      if len(dir_list)>29:
+        dir_list.clear()
 
-  else:
-   n = -1
-   dir_list.clear()
-   
+      if rs == 30 :
+        print('右に向かいます')
+        print('5秒後にカメラを閉じます')
+        sleep(5)
+        sys.exit()
+      elif ls == 30:
+        print('「  左に向かいます！  」')
+        print('「  5秒後にカメラを閉じます...  」')
+        sleep(5)
+        sys.exit()
+      else:
+            print('...')
