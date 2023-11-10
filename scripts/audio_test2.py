39f4633454ed50f1940f4986e7143a733fc05fbe
suzumiyawaki <suzu_sheep@icloud.com>
2023-11-10 16:07:01 +0900

    add: rospy

--- scripts/audio_test2.py
+++ scripts/audio_test2.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 from online_audio_kit import AudioKit
-#from std_msgs.msg import String
+from std_msgs.msg import String
+import rospy
+
 audio = AudioKit(language = 'ja')
 
 class AudioKit():
-  shiro = '白'
-  cha   = '茶色'
-  for text in audio.vosk():
-    if shiro in text:
-      print('しろです')
-      break
-    elif cha in text:
-      print('ちゃいろです')
-      break
 
-audio = AudioKit()
+  def __init__(self):
+    self.pub = rospy.Publisher("topic_color", String, queue_size=10)
 
-#if __name__ == "__main__":
+  def publish(self):
+    shiro = '白'
+    cha   = '茶色'
+    for text in audio.vosk():
+      if shiro in text:
+        print('しろです')
+        self.pub.publish("white")
+        break
+      elif cha in text:
+          print('ちゃいろです')
+          self.pub.publish("brown")
+          break
+
+if __name__ == "__main__":
+
+  rospy.init_node("audio_node")
+  
+  audio = AudioKit()
+  
+  rate = rospy.Rate(1)
+
+  while not rospy.is_shutdown():
+    # パブリッシュ
+    audio.publish()
+        
+    rate.sleep()
