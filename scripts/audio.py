ba33042e5dad6f530f11f1d6d968867550942bbb
reina <ryugureina314@gmail.com>
2023-11-12 10:54:48 +0000

    fix: node&topic name, audio recoginition

--- scripts/audio.py
+++ scripts/audio.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import rospy
+import time
 from std_msgs.msg import String
 from online_audio_kit import AudioKit
-from GptApi import GptApi
 
 def CreateAudioProcessingNode():
     # Configurable variables
-    node_name = 'audio_processing'
-    topic_name = 'audio_processing'
+    node_name = 'audio_node'
+    topic_name = 'topic_color'
 
     # Node publisher
     pub = rospy.Publisher(topic_name, String, queue_size=1)
@@ -16,19 +16,19 @@ def CreateAudioProcessingNode():
     # Create an instance of AudioKit
     audio = AudioKit('ja') # Option : AudioKit(language= 'ja' | 'en', openai_api_key=str)
     audio.tts("こんにちは。何色の紙袋をお持ちしましょうか?")
+    
+    for i, text in enumerate(audio.vosk()):
+        if("白" in text):
+            color = 'white'
+        elif("茶" in text):
+            color = 'brown'
+        else: 
+            if(i % 20 == 0):
+                audio.tts("すみません、聞き取れませんでした。もう一度言ってください。")
+            continue
+        break
 
-    counter = 0
-    while True:
-        recognized_text = audio.stt()
-        color = GptApi.ExtractColor(recognized_text)
-        if(color != 'brown' and color != 'white'):
-            audio.tts("すみません、聞き取れませんでした。もう一度言ってください。")
-            counter += 1
-            if(counter > 5): 
-                audio.tts("不具合が発生しました。プロセスを終了します。")
-                exit()
-        else: break  
-
+    time.sleep(2)
     audio.tts(f"わかりました。{'茶' if color == 'brown' else '白'}色の紙袋をお持ちします。")
 
     rospy.loginfo(f"Set bagColor to '{color}'")
