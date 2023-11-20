82cdaae77c236ae715c0541dd62f93d1f562d27d
reina <ryugureina314@gmail.com>
2023-11-20 21:16:33 +0900

    update: optimization

--- scripts/main.py
+++ scripts/main.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-import time
+
 from Controller import Controller
+
 
 def main():
     # Create an instance / node
     ctrl: Controller = Controller()
 
+    # Wait for color and direction being given
+    print('[Info] Listening to topic_color.')
     ctrl.ListenToTopicColor()
+    print('[Info] Listening to topic_direction.')
     ctrl.ListenToTopicDirection()
-    print('[Debug] Listening to topic_color and topic_direction.')
-
-    debug_counter = 0
-    # Wait for both color and direction to be specified
-    while(ctrl.bagColor == None or ctrl.direction == None):
-        print(f'[Debug] Waiting for color and direction. Time elapsed: {debug_counter}s')
-        time.sleep(1)
-        debug_counter = debug_counter + 1
     
+    # Move to the first target
     ctrl.PublishTopicMove(1 if(ctrl.direction == 'left') else 2) # Left to 1, right to 2
+    print('[Info] topic_move published.')
     ctrl.ReceiveTheBag()
-    time.sleep(3)
 
+    # Move to the second target
     ctrl.PublishTopicMove(2 if(ctrl.direction == 'left') else 1) # Move to the other side
+    print('[Info] topic_move published.')
     ctrl.GiveTheBag()
-    time.sleep(3)
 
-    ctrl.PublishTopicMove(0) # Return to the Origin
+    # Go back to the origin
+    ctrl.PublishTopicMove(0)
+    print('[Info] topic_move published.')
 
-    print('[Debug] Program terminated.')
+    print('[Info] main_node has been terminated.')
     
 
 if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print(f"An exception has been occurred during the execution: {e}")
+        print(f"[Error] Exception occurred: {e}")
