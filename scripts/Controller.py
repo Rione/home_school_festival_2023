03af4c118845e5f20368a160c6c420480384a1a9
reina <ryugureina314@gmail.com>
2023-11-20 21:15:02 +0900

    update: optimization

--- scripts/Controller.py
+++ scripts/Controller.py
@@ -9,8 +9,14 @@ class Controller():
     def __init__(self):
         # Create a node
         rospy.init_node('main_node', anonymous=True)
+
+        # Variables
         self.color: str = None    # 紙袋の色
         self.direction: str = None   # 進行方向
+
+        # Publishers
+        self.pub_mov = rospy.Publisher('topic_move', String, queue_size=1)
+        self.pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
     
     def ListenToTopicColor(self):
         """
@@ -28,38 +34,41 @@ class Controller():
         """
         Publish topic_move; move to the target
         """
-        pub_move = rospy.Publisher('topic_move', String, queue_size=1)
-        pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
         if(target == 0): 
-            result = 'origin'
-            pub_tts.publish("ホームに戻ります")
-            time.sleep(3)
-        elif(target == 1): result = 'target1'
-        elif(target == 2): result = 'target2'
-        else: rospy.logdebug('[Debug] Wrong argument given.')
+            data = 'origin'
+            self.pub_tts.publish("info_move_origin")
+        elif(target == 1): 
+            data = 'target1'
+            self.pub_tts.publish("info_move_target")
+        elif(target == 2): 
+            data = 'target2'
+            self.pub_tts.publish("info_move_target")
+        else: rospy.logerr('[Error] Wrong parameter given.')
 
-        pub_move.publish(result)
+        time.sleep(3)
+        self.pub_mov.publish(data)
 
+        # Wait until the robot arrives at the target location
         rospy.logdebug('[Debug] result: ' + rospy.wait_for_message('topic_end_nav', String, timeout=None))
 
     def ReceiveTheBag(self):
         """
         Handle the arrival at the 1st target.
         """
-        ## WIP
-        pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
-        pub_tts.publish("第一目標に到達しました。")
-        rospy.logdebug('[Debug] Arrived at the 1st designated target.')
-        rospy.logdebug('[Debug] Start navigation to the next target soon.')
+        rospy.loginfo('[Info] Arrived at the 1st target.')
+        rospy.loginfo('[Info] Start navigation to the next target soon.')
+        self.pub_tts.publish("info_arrive_target")
+        time.sleep(3)
+        self.pub_tts.publish(f"{'ask_white' if self.color == 'white' else 'ask_brown'}")
+        time.sleep(3)
 
     def GiveTheBag(self):
         """
         Handle the arrival at the 2nd target.
         """
-        ## WIP
-        pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
-        pub_tts.publish("第二目標に到達しました。")
-        rospy.logdebug('[Debug] Arrived at the 2nd designated target.')
-        rospy.logdebug('[Debug] Start navigation to the next target soon.')
+        rospy.loginfo('[Info] Arrived at the 2nd target.')
+        rospy.loginfo('[Info] Start navigation to the next target soon.')
+        self.pub_tts.publish("ask_bag")
+        time.sleep(3)
 
 
