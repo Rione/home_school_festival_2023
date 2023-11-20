eef003927a735ecc452d397bd1d2a11b9ff9f2a2
reina <ryugureina314@gmail.com>
2023-11-20 21:14:41 +0900

    update: support for tts

--- scripts/audio.py
+++ scripts/audio.py
@@ -5,35 +5,50 @@ import time
 from std_msgs.msg import String
 from online_audio_kit import AudioKit
 
+
 # Create an instance of AudioKit
 audio = AudioKit('ja') # Option : AudioKit(language= 'ja' | 'en', openai_api_key=str)
 
 def CreateAudioProcessingNode():
     # Node and publisher
-    pub = rospy.Publisher('topic_color', String, queue_size=1)
     rospy.init_node('audio_node', anonymous=True)
-   
-    audio.tts("こんにちは。何色の紙袋をお持ちしましょうか?")
+    pub = rospy.Publisher('topic_color', String, queue_size=1)
+
+    TextToSpeech({'data': 'ask_color'})
+
+    color = None
+    while(color == None):
+        try:
+            for text in audio.vosk():
+                if("白" in text):
+                    color = 'white'
+                    break
+                elif("茶" in text):
+                    color = 'brown'
+                    break
+                else: continue
+            # if the for-loop ends up without break
+            else: 
+                TextToSpeech({'data': 'err_ask_color'})
+                time.sleep(3)
+                continue
+        except Exception as e:
+            rospy.logerr(f'[Error] Exception occurred: {e}')
+        
+    rospy.logdebug(f"[Debug] Set color to: '{color}'")
+    TextToSpeech({'data': f"{'info_white' if color == 'white' else 'info_brown'}"})
+    time.sleep(3)
     
-    for i, text in enumerate(audio.vosk()):
-        if("白" in text):
-            color = 'white'
-        elif("茶" in text):
-            color = 'brown'
-        else: 
-            if(i % 20 == 0):
-                audio.tts("すみません、聞き取れませんでした。もう一度言ってください。")
-            continue
-        break
+    if(color != None):
+        pub.publish(color) # Publish a topic without waiting for the subscriber being established
+    else: rospy.logerr("[Error] The color is not given. Please try again.")
 
-    time.sleep(2)
-    audio.tts(f"わかりました。{'茶' if color == 'brown' else '白'}色の紙袋をお持ちします。")
-
-    rospy.loginfo(f"[Debug] Set color to: '{color}'")
-    pub.publish(color) # Publish a topic regardless of the subscriber already established or not
-
-def TextToSpeech(data):
-    audio.tts(data.data)
+def TextToSpeech(obj):
+    """
+    Take an object as the parameter; object.data = "{name_of_the_audio_file}" 
+    """
+    prefix = "../voice/" # Configure here to change directory to load audio from
+    audio.play(prefix + obj.data + '.mp3')
 
 if __name__ == '__main__':
     try:
