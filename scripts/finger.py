e3abe870de44f668013ac07cfa22f88888c3f850
reina <ryugureina314@gmail.com>
2023-11-20 21:15:24 +0900

    update: support for tts

--- scripts/finger.py
+++ scripts/finger.py
@@ -5,49 +5,48 @@ import time
 from std_msgs.msg import String
 from hand_detect import finger_direction
 
-# Configurable variables
-limit: int = 10     # How many times to receive data from hand_detect
 
 def CreateFingerDirectionNode() -> None:
     # Node and publisher
-    pub_direction = rospy.Publisher('topic_direction', String, queue_size=1)
-    pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
     rospy.init_node('finger_node', anonymous=True)
+    pub_dir = rospy.Publisher('topic_direction', String, queue_size=1)
+    pub_tts = rospy.Publisher('topic_tts', String, queue_size=1)
 
-    # Initialization
+    pub_tts.publish("ask_direction")
+    time.sleep(3)   # Wait for the audio to stop
+
+    limit: int = 10     # How many times to receive data from hand_detect
+
     counter_L = 0
     counter_R = 0
-
-    pub_tts("行きたい方向を指さしてください。")
-    time.sleep(3)
-
-    # Main
+    result = None
+    rospy.loginfo("[Info] Press ESC to exit.")
+    # args : camera_id (default: 0)
+    # yield : direction ("R" | "L" | None)
+    # ESC to exit
     for direction in finger_direction():
-        # args : camera_id (default: 0)
-        # yield : direction ("R" | "L" | None)
-        # ESC to exit
         if(direction):
             if(direction == 'L'):
                 counter_R = 0
                 counter_L += 1
+                if(counter_L >= limit):
+                    result =  'left'
+                    break
             elif(direction == 'R'):
                 counter_L = 0
                 counter_R += 1
-
-            if(counter_L >= limit):
-                result =  'left'
-                break
-            elif(counter_R >= limit):
-                result =  'right'
-                break
+                if(counter_R >= limit):
+                    result =  'right'
+                    break             
         else: # Reset counters
             counter_L = 0
             counter_R = 0
 
-    rospy.loginfo(f"[Debug] Set direction to: {result}")
-    pub_tts("わかりました。")
-    time.sleep(3)
-    pub_direction.publish(result) # Publish a topic regardless of the subscriber already established or not
+    rospy.logdebug(f"[Debug] Set direction to: {result}")
+
+    if(result != None):
+        pub_dir.publish(result) # Publish a topic without waiting for the subscriber being established
+    else: rospy.logerr("[Error] The direction is not given. Please try again.")
 
 if __name__ == '__main__':
     try:
