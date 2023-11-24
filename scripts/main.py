3e2a0f1a93a92b3df8ea9861989d1e6a433ce703
You8102 <yuko.tonami73@gmail.com>
2023-11-24 10:28:12 +0900

    fix

--- scripts/main.py
+++ scripts/main.py
@@ -13,6 +13,13 @@ rospy.Subscriber("topic_direction",String,)
 rospy.Subscriber("topic_fin",String,)
 PUB="***" #なんでもいい
 
+
+def res_check():
+    while(1):
+        res=audio.stt()#紙袋受け渡し/受け取り確認
+        if "OK" in res:
+            break
+
 if __name__ == '__main__':
     try:
         for i in range(10):
@@ -27,6 +34,11 @@ if __name__ == '__main__':
         target=rospy.wait_for_message("topic_direction", String)#finger終了まで待機
         print("色と方向がわかりました")
         print(Color,target)
+        
+        if Color.data=="white":
+            Color="しろ色"
+        elif Color.data=="brown":
+            Color="茶色"
 
 
         if  target=="right":#１回目移動目標設定
@@ -40,14 +52,9 @@ if __name__ == '__main__':
         rospy.wait_for_message("topic_fin", String)#移動１回目終了まで待機
         print("finトピック受信")
 
-        audio.tts(f"{str(Color)}の紙袋をください")
+        audio.tts(f"{Color}の紙袋をください")
 
-        
-        while(1):
-            res=audio.stt()#紙袋受け取り確認
-            if "OK" in res:
-                break
-        
+        res_check()#紙袋受け渡し確認
         
         if  target=="target1":#２回目移動目標設定
             target="target2"
@@ -59,12 +66,9 @@ if __name__ == '__main__':
         time.sleep(0.1)
         rospy.wait_for_message("topic_fin", String)#移動２回目終了まで待機
 
-        audio.tts(f"{str(Color)}の紙袋を持ってきました")
+        audio.tts(f"{Color}の紙袋を持ってきました")
         
-        while(1):
-            res=audio.stt()#紙袋受け渡し確認
-            if "OK" in res:
-                break
+        res_check()#紙袋受け取り確認
 
         pub_send_goal.publish("origin")#移動３回目起動
         time.sleep(0.1)
