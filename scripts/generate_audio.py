dc984467ab74d595e439d041e0e4ce0955154491
reina <ryugureina314@gmail.com>
2023-11-24 20:29:48 +0900

    fix: pronunciation SHIRO

--- scripts/generate_audio.py
+++ scripts/generate_audio.py
@@ -7,9 +7,9 @@ prefix = "../voice/" # Configure here to change save location
 audio = AudioKit('ja')
 
 audio.tts("何色の紙袋をお持ちしましょうか?", mode="gen", path=prefix + "ask_color.mp3")
-audio.tts("わかりました。白色の紙袋をお持ちします。", mode="gen", path=prefix + "info_white.mp3")
+audio.tts("わかりました。しろ色の紙袋をお持ちします。", mode="gen", path=prefix + "info_white.mp3")
 audio.tts("わかりました。茶色の紙袋をお持ちします。", mode="gen", path=prefix + "info_brown.mp3")
-audio.tts("白色の紙袋をのせてください。", mode="gen", path=prefix + "ask_white.mp3")
+audio.tts("しろ色の紙袋をのせてください。", mode="gen", path=prefix + "ask_white.mp3")
 audio.tts("茶色の紙袋をのせてください。", mode="gen", path=prefix + "ask_brown.mp3")
 audio.tts("紙袋をお取りください。", mode="gen", path=prefix + "ask_bag.mp3")
 audio.tts("目標地点へ移動します。", mode="gen", path=prefix + "info_move_target.mp3")
