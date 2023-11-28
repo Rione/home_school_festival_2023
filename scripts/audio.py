d28ffb288d849a05c14679ff7029dcc04ad719cf
reina <ryugureina314@gmail.com>
2023-11-28 20:24:45 +0900

    update: loop publish

--- scripts/audio.py
+++ scripts/audio.py
@@ -2,22 +2,26 @@
 
 import rospy
 import time
-import subprocess
+import threading
 from std_msgs.msg import String
 from online_audio_kit import AudioKit
 
 
-def CreateAudioProcessingNode(audio):
+# Create an instance of AudioKit
+AUDIO = AudioKit('ja') # Option : AudioKit(language= 'ja' | 'en', openai_api_key=str)
+
+def CreateAudioProcessingNode():
     # Node and publisher
     rospy.init_node('audio_node', anonymous=True)
     pub = rospy.Publisher('topic_color', String, queue_size=1)
+    rate = rospy.Rate(1)
 
-    TextToSpeech({'data': 'ask_color'}, audio)
+    TextToSpeech("ask_color")
 
     color = None
     while(color == None):
         try:
-            for text in audio.vosk():
+            for text in AUDIO.vosk():
                 if("白" in text):
                     color = 'white'
                     break
@@ -27,38 +31,48 @@ def CreateAudioProcessingNode(audio):
                 else: continue
             # if the for-loop ends up without break
             else: 
-                TextToSpeech({'data': 'err_ask_color'}, audio)
+                TextToSpeech("err_ask_color")
                 time.sleep(3)
                 continue
         except Exception as e:
             rospy.loginfo(f'[Error] Exception occurred: {e}')
         
     rospy.loginfo(f"[Debug] Set color to: '{color}'")
-    TextToSpeech({'data': f"{'info_white' if color == 'white' else 'info_brown'}"}, audio)
+    time.sleep(2)
+    if(color == 'white'):
+        TextToSpeech("info_white")
+    elif(color == 'brown'):
+        TextToSpeech("info_brown")
     time.sleep(3)
     
-    if(color != None):
-        pub.publish(color) # Publish a topic without waiting for the subscriber being established
-    else: rospy.loginfo("[Error] The color is not given. Please try again.")
+    try:
+        while not rospy.is_shutdown():
+            pub.publish(color) # Publish a topic without waiting for the subscriber being established
+            rate.sleep()
+    except Exception as e: 
+        rospy.loginfo(f"[Error] Exception occurred: {e}")
 
-def TextToSpeech(obj, audio):
+def TextToSpeech(obj):
     """
     Take an object as the parameter; object.data = "{name_of_the_audio_file}" 
     """
-    prefix = (subprocess.run("find ~/catkin_ws/src/home_school_festival_2023 -name voice", shell=True, capture_output=True, text=True)).stdout + '/' # Configure here to change directory to load audio from
+    prefix = "" # Absolute path of voice directory
     try:
-        audio.play(prefix + obj["data"] + '.mp3')
-    except TypeError:
-        audio.play(prefix + obj.data + '.mp3')
+        AUDIO.play(prefix + obj + '.mp3')
+    except Exception:
+        AUDIO.play(prefix + obj.data + '.mp3')
+
+def ListenToTopicTTS():
+    rospy.Subscriber("topic_tts", String, TextToSpeech)
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
-        # Create an instance of AudioKit
-        audio = AudioKit('ja') # Option : AudioKit(language= 'ja' | 'en', openai_api_key=str)
+        thread1 = threading.Thread(target = CreateAudioProcessingNode)
+        thread2 = threading.Thread(target = ListenToTopicTTS)
 
-        CreateAudioProcessingNode()
-        rospy.Subscriber("topic_tts", String, TextToSpeech)
-        rospy.spin()
+        thread1.start()
+        thread2.start()
     except rospy.ROSInterruptException:
         pass
 
