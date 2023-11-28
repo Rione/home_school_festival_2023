1622849586c0fb3d750ca805bdbb56cbdf0a347b
reina <ryugureina314@gmail.com>
2023-11-28 22:05:41 +0900

    feat: load env

--- scripts/generate_audio.py
+++ scripts/generate_audio.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
 # Run this program to generate voice data as mp3.
+import os
+from dotenv import load_dotenv
 from online_audio_kit import AudioKit
 
-prefix = ""
+load_dotenv()
+PREFIX = os.getenv('VOICE_PATH')
 
 audio = AudioKit('ja')
-audio.tts("何色の紙袋をお持ちしましょうか?", mode="gen", path=prefix + "ask_color.mp3")
-audio.tts("わかりました。しろ色の紙袋をお持ちします。", mode="gen", path=prefix + "info_white.mp3")
-audio.tts("わかりました。茶色の紙袋をお持ちします。", mode="gen", path=prefix + "info_brown.mp3")
-audio.tts("しろ色の紙袋をのせてください。", mode="gen", path=prefix + "ask_white.mp3")
-audio.tts("茶色の紙袋をのせてください。", mode="gen", path=prefix + "ask_brown.mp3")
-audio.tts("紙袋をお取りください。", mode="gen", path=prefix + "ask_bag.mp3")
-audio.tts("目標地点へ移動します。", mode="gen", path=prefix + "info_move_target.mp3")
-audio.tts("目標地点へ到達しました。", mode="gen", path=prefix + "info_arrive_target.mp3")
-audio.tts("ホームへ戻ります。", mode="gen", path=prefix + "info_move_origin.mp3")
-audio.tts("すみません、聞き取れませんでした。もう一度言ってください。", mode="gen", path=prefix + "err_ask_color.mp3")
-audio.tts("移動する方向を指さしてください。", mode="gen", path=prefix + "ask_direction.mp3")
+audio.tts("何色の紙袋をお持ちしましょうか?", mode="gen", path=PREFIX + "ask_color.mp3")
+audio.tts("わかりました。しろ色の紙袋をお持ちします。", mode="gen", path=PREFIX + "info_white.mp3")
+audio.tts("わかりました。茶色の紙袋をお持ちします。", mode="gen", path=PREFIX + "info_brown.mp3")
+audio.tts("しろ色の紙袋をのせてください。", mode="gen", path=PREFIX + "ask_white.mp3")
+audio.tts("茶色の紙袋をのせてください。", mode="gen", path=PREFIX + "ask_brown.mp3")
+audio.tts("紙袋をお取りください。", mode="gen", path=PREFIX + "ask_bag.mp3")
+audio.tts("目標地点へ移動します。", mode="gen", path=PREFIX + "info_move_target.mp3")
+audio.tts("目標地点へ到達しました。", mode="gen", path=PREFIX + "info_arrive_target.mp3")
+audio.tts("ホームへ戻ります。", mode="gen", path=PREFIX + "info_move_origin.mp3")
+audio.tts("すみません、聞き取れませんでした。もう一度言ってください。", mode="gen", path=PREFIX + "err_ask_color.mp3")
+audio.tts("移動する方向を指さしてください。", mode="gen", path=PREFIX + "ask_direction.mp3")
 
--- scripts/generate_audio_new.py
+++ scripts/generate_audio_new.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-
+import os
+from dotenv import load_dotenv
 from speech_and_NLP.src.tools.text_to_speech.textToWav import textToWav
 
-PRE = "" 
+
+load_dotenv()
+PREFIX = os.getenv('VOICE_PATH')
 
 if __name__ == '__main__':
     try:
-        textToWav(speed=1.0, path=PRE+'ask_color.wav', text='何色の紙袋をお持ちしましょうか?')
-        textToWav(speed=1.0, path=PRE+'ask_direction.wav', text='移動する方向を指さしてください。')
-        textToWav(speed=1.0, path=PRE+'ask_white.wav', text='しろ色の紙袋をのせてください。')
-        textToWav(speed=1.0, path=PRE+'ask_brown.wav', text='茶色の紙袋をのせてください。')
-        textToWav(speed=1.0, path=PRE+'ask_bag.wav', text='紙袋をお取りください。')
-        textToWav(speed=1.0, path=PRE+'info_white.wav', text='わかりました。しろ色の紙袋をお持ちします。')
-        textToWav(speed=1.0, path=PRE+'info_brown.wav', text='わかりました。茶色の紙袋をお持ちします。')
-        textToWav(speed=1.0, path=PRE+'info_move_target.wav', text='目標地点へ移動します。')
-        textToWav(speed=1.0, path=PRE+'info_arrive_target.wav', text='目標地点へ到達しました。')
-        textToWav(speed=1.0, path=PRE+'info_move_origin.wav', text='ホームへ戻ります。')
-        textToWav(speed=1.0, path=PRE+'err_ask_color.wav', text='すみません、聞き取れませんでした。もう一度言ってください。')
+        textToWav(speed=1.0, path=PREFIX+'ask_color.wav', text='何色の紙袋をお持ちしましょうか?')
+        textToWav(speed=1.0, path=PREFIX+'ask_direction.wav', text='移動する方向を指さしてください。')
+        textToWav(speed=1.0, path=PREFIX+'ask_white.wav', text='しろ色の紙袋をのせてください。')
+        textToWav(speed=1.0, path=PREFIX+'ask_brown.wav', text='茶色の紙袋をのせてください。')
+        textToWav(speed=1.0, path=PREFIX+'ask_bag.wav', text='紙袋をお取りください。')
+        textToWav(speed=1.0, path=PREFIX+'info_white.wav', text='わかりました。しろ色の紙袋をお持ちします。')
+        textToWav(speed=1.0, path=PREFIX+'info_brown.wav', text='わかりました。茶色の紙袋をお持ちします。')
+        textToWav(speed=1.0, path=PREFIX+'info_move_target.wav', text='目標地点へ移動します。')
+        textToWav(speed=1.0, path=PREFIX+'info_arrive_target.wav', text='目標地点へ到達しました。')
+        textToWav(speed=1.0, path=PREFIX+'info_move_origin.wav', text='ホームへ戻ります。')
+        textToWav(speed=1.0, path=PREFIX+'err_ask_color.wav', text='すみません、聞き取れませんでした。もう一度言ってください。')
     except Exception as e:
         print(f"[Debug] Exception occured: {e}")
