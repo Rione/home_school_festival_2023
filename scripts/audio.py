3ab5fe18e53bc5b5f4affe9251535a988227d6b0
reina <ryugureina314@gmail.com>
2023-11-30 16:52:28 +0900

    update: unify topics

--- scripts/audio.py
+++ scripts/audio.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+ENV_PATH = ""
+
 import rospy
 import time
 import os
@@ -8,19 +10,12 @@ from std_msgs.msg import String
 from online_audio_kit import AudioKit
 
 
+load_dotenv(dotenv_path=ENV_PATH)
 # Create an instance of AudioKit
 AUDIO = AudioKit('ja') # Option : AudioKit(language= 'ja' | 'en', openai_api_key=str)
 
-load_dotenv()
-
-def CreateAudioProcessingNode():
-    # Node and publisher
-    rospy.init_node('audio_node', anonymous=True)
-    pub = rospy.Publisher('topic_color', String, queue_size=10)
-
-    TextToSpeech("ask_color")
-
-    color = None
+def SpeechRecognition() -> str:
+    color:str = None
     while(color == None):
         try:
             for text in AUDIO.vosk():
@@ -33,27 +28,71 @@ def CreateAudioProcessingNode():
                 else: continue
             # if the for-loop ends up without break
             else: 
-                TextToSpeech("err_ask_color")
+                TextToSpeech("err_audio")
                 time.sleep(3)
                 continue
         except Exception as e:
             rospy.loginfo(f'[Error] Exception occurred: {e}')
+    
+    return color
+
+def AskWhichColor() -> None:
+    pub = rospy.Publisher('topic_color', String, queue_size=1)
+
+    TextToSpeech("ask_color")
+
+    boolean:str = 'no'
+    while(boolean == 'no'):
+        color = SpeechRecognition()
+        if(color == 'white'):
+            TextToSpeech("info_white")
+        elif(color == 'brown'):
+            TextToSpeech("info_brown")
+        time.sleep(3)
+        boolean = AskYesOrNo()
         
     rospy.loginfo(f"[Debug] Set color to: '{color}'")
     time.sleep(2)
-
-    if(color == 'white'):
-        TextToSpeech("info_white")
-    elif(color == 'brown'):
-        TextToSpeech("info_brown")
-    time.sleep(3)
     
     try:
         pub.publish(color) # Publish a topic without waiting for the subscriber being established
     except Exception as e: 
         rospy.loginfo(f"[Error] Exception occurred: {e}")
 
-def TextToSpeech(obj):
+    return
+
+def AskYesOrNo() -> str:
+    pub = rospy.Publisher('topic_end_yes', String, queue_size=10)
+
+    TextToSpeech("ask_yes_or_no")
+
+    boolean:bool = None
+    while(boolean == None):
+        try:
+            for text in AUDIO.vosk():
+                if("はい" in text):
+                    boolean = 'yes'
+                    break
+                elif("いいえ" in text):
+                    boolean = 'no'
+                    break
+                else: continue
+            # if the for-loop ends up without break
+            else: 
+                TextToSpeech("err_audio")
+                time.sleep(3)
+                continue
+        except Exception as e:
+            rospy.loginfo(f'[Error] Exception occurred: {e}')
+    
+    try:
+        pub.publish(boolean) # Publish a topic without waiting for the subscriber being established
+    except Exception as e: 
+        rospy.loginfo(f"[Error] Exception occurred: {e}")
+
+    return boolean
+
+def TextToSpeech(obj) -> None:
     """
     Take an object as the parameter; object.data = "{name_of_the_audio_file}" 
     """
@@ -61,18 +100,28 @@ def TextToSpeech(obj):
     prefix = os.getenv('VOICE_PATH')
     try:
         AUDIO.play(prefix + obj + '.mp3')
-    except Exception:
-        AUDIO.play(prefix + obj.data + '.mp3')
+    except Exception as e:
+        rospy.loginfo(f"[Error] Exception occurred: {e}")
 
-def ListenToTopicTTS():
-    rospy.Subscriber("topic_tts", String, TextToSpeech)
+    return
+
+def Router(data) -> None:
+    signal = data.data
+    if(signal == "start_mic"):
+        AskWhichColor()
+    elif(signal == "start_yes"):
+        AskYesOrNo()
+    else:
+        TextToSpeech(data)
+
+def main() -> None:
+    rospy.init_node('audio_node', anonymous=True)
+    rospy.Subscriber("topic_audio", String, Router)
     rospy.spin()
 
 if __name__ == '__main__':
     try:
-        CreateAudioProcessingNode()
-        rospy.loginfo("[Debug] passed.")
-        ListenToTopicTTS()
+        main()        
     except rospy.ROSInterruptException:
         pass
 
