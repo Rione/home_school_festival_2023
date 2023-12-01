c49821c15c755a822ff6c519b4e23b1b13aea703
reina <ryugureina314@gmail.com>
2023-12-01 20:41:29 +0900

    fix

--- scripts/tts.py
+++ scripts/tts.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 
-ENV_PATH = "/home/yuko001/catkin_ws/src/home_school_festival_2023/.env"
-
 import rospy
-import os
-from dotenv import load_dotenv
+import env
 from std_msgs.msg import String
 from playsound import playsound
 
 
-load_dotenv(dotenv_path=ENV_PATH)
-
 def TextToSpeech(data) -> None:
     rospy.loginfo("[Debug] tts called.")
-    prefix = os.getenv('VOICE_PATH')
+    prefix = env.VOICE_PATH
     try:
         playsound(prefix + data.data + '.mp3')
     except Exception as e:
